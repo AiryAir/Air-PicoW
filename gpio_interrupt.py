5c98a4b3d28b01f6b9cd98eb122556dac858317d
Pratham <69145482+AiryAir@users.noreply.github.com>
2024-04-23 01:00:49 +0530

    debounced buttons

--- gpio_interrupt.py
+++ gpio_interrupt.py
@@ -1,8 +1,21 @@
 from machine import Pin
-counter=0
+import utime
+
+counter = 0
 pin = Pin(5, Pin.IN, Pin.PULL_UP)
+
+def debounce(pin):
+    debounce_time = 50
+    current_value = pin.value()
+    utime.sleep_ms(debounce_time)
+    if pin.value() == current_value:
+        return True
+    return False
+
 while True:
-    if pin.value()==0:
+    if debounce(pin) == False:
+        continue
+    if pin.value() == 0:
         print("Button Pressed")
-        counter+=1
-        print("Count={}".format(counter))
+        counter += 1
+        print("Count = {}".format(counter))
